ceef56e33da15b82981556b5dd27fb146f6b0648
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-15 13:54:31 +0400

    initial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string sen = "    I loVE ProgramINg   ";
+               
+               
+
+            Console.WriteLine(MakeCorrect(sen));
+        }
+        public static string MakeCorrect (string str)
+        {
+            string[] arr = Regex.Split(@"/s+", " ");
+
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].Append(Char.ToUpper(arr[i][0]));
+
+                for (int j = 1; j < arr[i].Length; j++)
+                {
+                    arr[i].Append(Char.ToLower(arr[i][j]));
+                }
+            }
+
+            return string.Join(" ", arr);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Product
+    {
+        public string ID;
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int Income;
+        public int Count;
+        public Product(string brandName, string model , int price, int cost, int count)
+        {
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Count = count;
+            Price = 0;
+
+            if (count < 0)
+            {
+                count = 0;
+            }
+
+            
+        }
+    }
+}
